caf8e1eb6be099004ccc9b4aa9029b3a4c2a09f0
mariana diaz <chetos_libra30@hotmail.com>
2012-05-08 13:14:42 -0500

    otravez

--- tweet/main/forms.py
+++ tweet/main/forms.py
@@ -4,8 +4,10 @@ from main.models import User, Tweet
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget = forms.PasswordInput)
+
 	class Meta: 
 		model = User
+		readonly_fields=('nick_name',)
 
 class TweetForm(forms.ModelForm):
 	class Meta: 
